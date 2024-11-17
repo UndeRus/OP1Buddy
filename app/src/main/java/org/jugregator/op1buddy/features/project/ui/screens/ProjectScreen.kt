8cad86cb5d24fabe45f81c6d789c5dd4b05a77f9
Kerrigan <xybermind@gmail.com>
2024-11-17 20:49:00 +0300

    Fix projects item

--- app/src/main/java/org/jugregator/op1buddy/features/project/ui/screens/ProjectScreen.kt
+++ app/src/main/java/org/jugregator/op1buddy/features/project/ui/screens/ProjectScreen.kt
@@ -1,23 +1,30 @@
 package org.jugregator.op1buddy.features.project.ui.screens
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.jugregator.op1buddy.R
 import org.jugregator.op1buddy.data.synth.SynthEngine
 import org.jugregator.op1buddy.features.project.ProjectScreenViewModel
 import org.jugregator.op1buddy.features.project.ui.views.ProjectBottomBar
@@ -28,10 +35,44 @@ import org.koin.androidx.compose.koinViewModel
 fun ProjectScreen(
     modifier: Modifier = Modifier,
     viewModel: ProjectScreenViewModel = koinViewModel(),
+    onBackClicked: () -> Unit,
     onSyncClicked: (String) -> Unit,
     onDrumKitSelected: (String, Int) -> Unit,
 ) {
     val uiState by viewModel.state.collectAsState()
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            ProjectAppBar(
+                title = uiState.title,
+                onBackClicked = onBackClicked,
+                onMoreClicked = {},
+            )
+        }, bottomBar = {
+            ProjectBottomBar(
+                selectedTab = uiState.tabOpened
+            ) {
+                if (it == ProjectTab.Sync) {
+                    onSyncClicked(uiState.projectId)
+                } else {
+                    viewModel.selectTab(it)
+                }
+            }
+        }) { innerPadding ->
+        val items by remember { derivedStateOf { uiState.items } }
+        val lazyColumnState = rememberLazyListState()
+        LazyColumn(modifier = Modifier.padding(innerPadding), state = lazyColumnState) {
+            items(items, key = { it.filename }) {
+                ProjectResourceItem(it, {
+                    if (it is ProjectResource.Drumkit) {
+                        onDrumKitSelected(uiState.projectId, it.index)
+                    }
+                })
+            }
+        }
+    }
+
+    /*
     Column(modifier = modifier) {
         if (uiState.titleEditEnabled) {
             Row {
@@ -55,28 +96,9 @@ fun ProjectScreen(
                     }, text = uiState.title.ifEmpty { "..." })
             }
         }
-
-        //val items by remember { derivedStateOf { uiState.items } }
-        val lazyColumnState = rememberLazyListState()
-        LazyColumn(modifier = Modifier.weight(1f), state = lazyColumnState) {
-            items(uiState.items, key = { it.filename }) {
-                ProjectResourceItem(it, {
-                    if (it is ProjectResource.Drumkit) {
-                        onDrumKitSelected(uiState.projectId, it.index)
-                    }
-                })
-            }
-        }
-
-        ProjectBottomBar(
-            selectedTab = uiState.tabOpened) {
-            if (it == ProjectTab.Sync) {
-                onSyncClicked(uiState.projectId)
-            } else {
-                viewModel.selectTab(it)
-            }
-        }
     }
+
+     */
 }
 
 sealed class ProjectResource(val index: Int, val filename: String) {
@@ -98,6 +120,61 @@ enum class ProjectTab {
 @Composable
 fun ProjectScreenPreview() {
     MaterialTheme {
-        ProjectScreen(onSyncClicked = {}, onDrumKitSelected = { projectId, drumkitId -> })
+        ProjectScreen(
+            onBackClicked = {},
+            onSyncClicked = {},
+            onDrumKitSelected = { projectId, drumkitId -> },
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProjectAppBar(modifier: Modifier = Modifier, title: String, onBackClicked: () -> Unit, onMoreClicked: () -> Unit) {
+    TopAppBar(
+        modifier = modifier,
+        title = {
+            Text(
+                modifier = Modifier.padding(start = 12.dp).basicMarquee(),
+                text = title,
+                style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Black)
+            )
+        },
+        navigationIcon = {
+            IconButton(
+                modifier = Modifier.padding(start = 8.dp),
+                onClick = onBackClicked
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(20.dp),
+                    painter = painterResource(R.drawable.appbar_back),
+                    tint = MaterialTheme.colorScheme.onSurface,
+                    contentDescription = null
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                modifier = Modifier.padding(end = 8.dp),
+                onClick = onMoreClicked
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(20.dp),
+                    painter = painterResource(R.drawable.appbar_more),
+                    tint = MaterialTheme.colorScheme.onSurface,
+                    contentDescription = null
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun ProjectAppBarPreview() {
+    MaterialTheme {
+        ProjectAppBar(title = "Project name", onBackClicked = {}, onMoreClicked = {})
     }
 }
